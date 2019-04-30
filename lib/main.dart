dee9e513b5b3cb3f62e2092dc196a53880401e92
Cory Ingram <cory.ingram@qhrtech.com>
2019-04-30 15:18:54 -0700

    Add some view things to main.

--- lib/main.dart
+++ lib/main.dart
@@ -23,29 +23,105 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _count = 0;
-
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sample Code'),
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
+        children: <Widget>[
+          _buildHpBar(),
+          _buildButtonRow(),
+          _buildListView()
+        ],
       ),
-      body: Center(
-        child: Text('You have pressed the button $_count times.'),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 50.0,
+    ));
+  }
+
+  _buildHpBar() {
+    return Row(
+      children: <Widget>[
+        Container(
+          height: 20,
+          width: MediaQuery.of(context).size.width,
+          child: Text(
+            ' HP ',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          decoration: new BoxDecoration(
+              gradient: new LinearGradient(
+                  colors: [Colors.red, Colors.red],
+                  begin: const FractionalOffset(0.0, 0.0),
+                  end: const FractionalOffset(0.5, 0.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp)),
         ),
+      ],
+    );
+  }
+
+  Widget _buildButtonRow() {
+    return Row(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: () => print('test'),
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+        RaisedButton(
+//                onPressed: () => _movePlayer(MapUtil.DIRECTIONS.NORTH),
+          child: Icon(
+            Icons.arrow_upward,
+            color: Colors.black,
+          ),
+        ),
+        RaisedButton(
+//                onPressed: () => _movePlayer(MapUtil.DIRECTIONS.SOUTH),
+          child: Icon(
+            Icons.arrow_downward,
+            color: Colors.black,
+          ),
+        ),
+        RaisedButton(
+//                onPressed: () => _movePlayer(MapUtil.DIRECTIONS.EAST),
+          child: Icon(
+            Icons.arrow_forward,
+            color: Colors.black,
+          ),
+        ),
+        Expanded(
+          child: RaisedButton(
+//                onPressed: _contextButton,
+            child: Icon(
+              Icons.face,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildListView() {
+    return Flexible(
+      child: ListView(
+        children: <Widget>[
+          Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: Container(
+                color: Colors.blue,
+                padding: EdgeInsets.all(2.0),
+                child: ListTile(
+                  title: Text('title'),
+                  subtitle: Text('Subtitle'),
+                  contentPadding: EdgeInsets.all(0.0),
+                  dense: true,
+                ),
+              )),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-              _count++;
-            }),
-        tooltip: 'Increment Counter',
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }

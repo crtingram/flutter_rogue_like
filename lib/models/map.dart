a7b83037fc8feb70c6444a8c81217d006067f5a3
Cory Ingram <cory.ingram@qhrtech.com>
2019-05-01 10:19:49 -0700

    Some convinience methods for map.

--- lib/models/map.dart
+++ lib/models/map.dart
@@ -2,16 +2,31 @@ import './map-tile.dart';
 import './map-data.dart';
 import './entities/entity.dart';
 import './entities/player.dart';
+import 'package:flutter_rogue_like/models/coordinate.dart';
 
 class Map {
   static final num width = 9;
   static final num height = 10;
 
   List<MapTile> _mapTiles;
+
   List<MapTile> get mapTiles => _mapTiles;
 
   Player player;
 
-  Map() : _mapTiles = MapData().getMapData(width, height),
-  player = new Player(0,0);
+  Map()
+      : _mapTiles = MapData().getMapData(width, height),
+        player = new Player(0, 0);
+
+  bool isValidCoordinate(Coordinate c) {
+    return c.x >= 0 && c.x < width && c.y >= 0 && c.y < height;
+  }
+
+  MapTile operator [](Coordinate c) {
+    return _mapTiles[c.x + c.y * width];
+  }
+
+  void operator []=(Coordinate c, MapTile tile) {
+    _mapTiles[c.x + c.y * width] = tile;
+  }
 }
